38debf74b2a05ef940a5c024b49c990e1f543b22
MatiasM <matias_monasterio@outlook.com>
2020-11-13 18:06:57 -0300

    Add lessee controller, login view and styles

---
+++ Controllers/LesseeController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlquilerApp.Controllers
+{
+    public class LesseeController : Controller
+    {
+        public LesseeController()
+        { }
+        public IActionResult index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
