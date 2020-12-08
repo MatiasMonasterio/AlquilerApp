2e699ac212ddc7c6ac310ea98704ed4f097b3ee1
MatiasM <matias_monasterio@outlook.com>
2020-12-07 21:55:30 -0300

    Initial migracion and build db

---
+++ Migrations/20201208005211_InitialDb.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlquilerApp.Migrations
+{
+    public partial class InitialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lessee",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Lastname = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    Telephone = table.Column<string>(type: "TEXT", nullable: true),
+                    Theme = table.Column<bool>(type: "INTEGER", nullable: false),
+                    FeeEmitAlert = table.Column<bool>(type: "INTEGER", nullable: false),
+                    FeeOverdueAlert = table.Column<bool>(type: "INTEGER", nullable: false),
+                    PaymentTicket = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lessee", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Renter",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Lastname = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Telephone = table.Column<string>(type: "TEXT", nullable: true),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    Theme = table.Column<bool>(type: "INTEGER", nullable: false),
+                    FeeEmitAlert = table.Column<bool>(type: "INTEGER", nullable: false),
+                    FeeOverdueAlert = table.Column<bool>(type: "INTEGER", nullable: false),
+                    PaymentTicket = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Renter", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Address = table.Column<string>(type: "TEXT", nullable: false),
+                    Number = table.Column<int>(type: "INTEGER", nullable: false),
+                    Town = table.Column<string>(type: "TEXT", nullable: false),
+                    provincia = table.Column<string>(type: "TEXT", nullable: false),
+                    age = table.Column<int>(type: "INTEGER", nullable: false),
+                    state = table.Column<bool>(type: "INTEGER", nullable: false),
+                    description = table.Column<string>(type: "TEXT", nullable: true),
+                    LesseeId = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Department_Lessee_LesseeId",
+                        column: x => x.LesseeId,
+                        principalTable: "Lessee",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contract",
+                columns: table => new
+                {
+                    LesseeId = table.Column<long>(type: "INTEGER", nullable: false),
+                    DepartmentId = table.Column<long>(type: "INTEGER", nullable: false),
+                    RenterId = table.Column<long>(type: "INTEGER", nullable: false),
+                    InitialDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contract", x => new { x.LesseeId, x.DepartmentId });
+                    table.ForeignKey(
+                        name: "FK_Contract_Department_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Department",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Contract_Lessee_LesseeId",
+                        column: x => x.LesseeId,
+                        principalTable: "Lessee",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Contract_Renter_RenterId",
+                        column: x => x.RenterId,
+                        principalTable: "Renter",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contract_DepartmentId",
+                table: "Contract",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contract_RenterId",
+                table: "Contract",
+                column: "RenterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Department_LesseeId",
+                table: "Department",
+                column: "LesseeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contract");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+
+            migrationBuilder.DropTable(
+                name: "Renter");
+
+            migrationBuilder.DropTable(
+                name: "Lessee");
+        }
+    }
+}
